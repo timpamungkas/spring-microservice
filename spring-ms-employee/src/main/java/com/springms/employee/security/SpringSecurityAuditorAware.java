1fdfb4fc093577c5539ec4056a4e3bf78b7a759a
timpamungkas <timotius.pamungkas@gmail.com>
2016-11-22 11:23:02 +0700

    Initial commit, no entity yet

---
+++ spring-ms-employee/src/main/java/com/springms/employee/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,19 @@
+package com.springms.employee.security;
+
+import com.springms.employee.config.Constants;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of AuditorAware based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public String getCurrentAuditor() {
+        String userName = SecurityUtils.getCurrentUserLogin();
+        return (userName != null ? userName : Constants.SYSTEM_ACCOUNT);
+    }
+}
---
+++ spring-ms-employee/src/main/webapp/app/components/form/pagination.constants.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('employeeApp')
+        .constant('paginationConstants', {
+            'itemsPerPage': 20
+        });
+})();
